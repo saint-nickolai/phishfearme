8d16d298d15c19452ffe5fa6556853853e68c7b6
Nick Howard <nick@Nicks-MacBook-Air.local>
2024-04-10 10:37:28 -0600

    new file:   Core/App.php 	new file:   Core/Container.php 	new file:   Core/Response.php 	new file:   Core/Router.php 	new file:   Core/ValidationException.php 	new file:   Core/Validator.php 	new file:   Core/functions.php 	new file:   Http/.DS_Store 	new file:   Http/Forms/LoginForm.php 	new file:   Http/controllers/.DS_Store 	new file:   Http/controllers/about.php 	new file:   Http/controllers/contact.php 	new file:   Http/controllers/index.php 	new file:   public/index.php 	new file:   routes.php 	new file:   views/403.php 	new file:   views/404.php 	new file:   views/about.view.php 	new file:   views/contact.view.php 	new file:   views/index.view.php 	new file:   views/partials/banner.php 	new file:   views/partials/footer.php 	new file:   views/partials/head.php 	new file:   views/partials/nav.php

---
+++ views/index.view.php
@@ -0,0 +1,53 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
+    </div>
+    <head>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', {'packages':['gauge']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+
+                var data = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    ['Humidity', 100],
+                    ['Temperature', 100],
+                    ['Moisture', 100]
+                ]);
+
+                var options = {
+                    width: 400, height: 120,
+                    greenFrom: 65, greenTo: 90,
+                    minorTicks: 3
+                };
+
+                var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+                chart.draw(data, options);
+            }
+        </script>
+    </head>
+    <body>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="mt-5 md:col-span-2 md:mt-0">
+                    <div class="shadow sm:overflow-hidden sm:rounded-md">
+                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                            <div id="chart_div" style="width: 400px; height: 160px;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</main>
+
+
+
+<?php require('partials/footer.php') ?>
